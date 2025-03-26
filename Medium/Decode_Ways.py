85f2f77885ccd195cb01186fe4545690fcb9b176
Nurlan Zhaparov <nurlanzhaparov888@gmail.com>
2025-03-26 00:25:21 -0500

    Create Decode_Ways.py

---
+++ Medium/Decode_Ways.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        if not s or s[0] == "0":
+            return 0
+
+        n = len(s)
+        dp = [0] * (n + 1)
+        dp[0], dp[1] = 1, 1
+
+        for i in range(1, n):
+            if s[i] != "0":
+                dp[i + 1] += dp[i]
+            if 10 <= int(s[i - 1 : i + 1]) <= 26:
+                dp[i + 1] += dp[i - 1]
+
+        return dp[n]
